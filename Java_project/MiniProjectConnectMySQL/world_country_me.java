f0ff1bdbc92dbbcc1afa4479a5e20b9aa278f3a3
JJongSSu <dlwnsrl8964@gmail.com>
2022-09-05 11:55:51 +0900

    second commit

--- Java_project/MiniProjectConnectMySQL/world_country_me.java
+++ Java_project/MiniProjectConnectMySQL/world_country_me.java
@@ -7,25 +7,24 @@ import java.sql.SQLException;
 import java.sql.Statement;
 
 public class world_country_me {
-    public static void main(String[] args){
+    public static void main(String[] args) {
         final String USER = "root";
         final String PASS = "tbrs00002b";
-        final String DB_URL = "jdbc:mysql://localhost/world";       
-        String QUERY = "select Code, Capital, Name, GNP " + "from country " + "where 1=1 " + "and Name like '%an%'";  
-        try{
+        final String DB_URL = "jdbc:mysql://localhost/world";
+        String QUERY = "select Code, Capital, Name, GNP " + "from country " + "where 1=1 " + "and Name like '%an%'";
+        try {
             Connection conn = DriverManager.getConnection(DB_URL, USER, PASS);
-            Statement stmt = conn.createStatement();   
-            ResultSet rs = stmt.executeQuery(QUERY);  
+            Statement stmt = conn.createStatement();
+            ResultSet rs = stmt.executeQuery(QUERY);
             while (rs.next()) {
                 System.out.print("Code: " + rs.getString("Code"));
                 System.out.print(", Capital: " + rs.getInt("Capital"));
                 System.out.print(", Name: " + rs.getString("Name"));
                 System.out.print(", GNP: " + rs.getString("GNP"));
-             }
-        }
-        catch (SQLException e){
+            }
+        } catch (SQLException e) {
             e.printStackTrace();
         }
-        return ;
+        return;
     }
 }
--- Java_project/MiniProjectConnectMysql.java
+++ Java_project/MiniProjectConnectMysql.java
@@ -5,15 +5,16 @@ import java.sql.SQLException;
 import java.sql.Statement;
 
 public class MiniProjectConnectMysql {
-    public static void main(String[] args){
+    public static void main(String[] args) {
         // VS code에서 폴더 만들기, class 파일 만들기
         // MySQL 열기
         // mysql-connector J(Java projects-referenced lib 연결)
         // connect to database 클릭
-        // connection method, hostname : localhost, port, username, password, default schema : 원하는 database 이름 -> 항목 입력확인
+        // connection method, hostname : localhost, port, username, password, default
+        // schema : 원하는 database 이름 -> 항목 입력확인
         final String USER = "root";
         final String PASS = "tbrs00002b";
-        final String DB_URL = "jdbc:mysql://localhost/world";       
+        final String DB_URL = "jdbc:mysql://localhost/world";
         // jdbc:mysql://localhost/world
         // jdbc: java database connection
         // jdbc:mysql(TCP/IP)
@@ -21,23 +22,25 @@ public class MiniProjectConnectMysql {
         // world(default schema)
         // connection 안되었을 때 예외처리 필요 -> try/catch 문 사용
         // -> pw입력 후 mysql 들어갔을 때까지의 명령어
-        String QUERY = "select ID, Name, Population " + "from city " + "where 1=1 " + "and Population >= 9230000";  // 끝나는 부분에 space 입력해줘야함
-        try{
+        String QUERY = "select ID, Name, Population " + "from city " + "where 1=1 " + "and Population >= 9230000"; // 끝나는
+                                                                                                                   // 부분에
+                                                                                                                   // space
+                                                                                                                   // 입력해줘야함
+        try {
             Connection conn = DriverManager.getConnection(DB_URL, USER, PASS);
-            Statement stmt = conn.createStatement();    // MySQL Query창 띄우는 명령어
+            Statement stmt = conn.createStatement(); // MySQL Query창 띄우는 명령어
             // 새로운 query 생성, 먼저 select 명령문 입력 후 test 해보기
-            ResultSet rs = stmt.executeQuery(QUERY);    // Query안에 명령어를 입력하기 위해서 stmt 사용(stmt가 Query창을 띄우는 class이기 때문에)
+            ResultSet rs = stmt.executeQuery(QUERY); // Query안에 명령어를 입력하기 위해서 stmt 사용(stmt가 Query창을 띄우는 class이기 때문에)
             // result grid, action output 확인 -> Line by line 방식으로 읽어주는 코드 작성필요(whlie문 사용)
             while (rs.next()) {
                 // Retrieve by column name, column datatype확인
                 System.out.print("ID: " + rs.getInt("ID"));
                 System.out.print(", Name: " + rs.getString("Name"));
                 System.out.print(", Population: " + rs.getInt("Population"));
-             }
-        }
-        catch (SQLException e){
+            }
+        } catch (SQLException e) {
             e.printStackTrace();
         }
-        return ;
+        return;
     }
 }
