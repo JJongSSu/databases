60812342f04ead14dd2788bf9b8db2d518bac844
JJongSSu <dlwnsrl8964@gmail.com>
2022-09-05 09:41:22 +0900

    first commit

---
+++ Java_project/MiniProjectConnectMysql.java
@@ -0,0 +1,10 @@
+public class MiniProjectConnectMysql {
+    Public static void main(String[] args){
+        // VS code에서 폴더 만들기, class 파일 만들기
+        // MySQL 열기
+        // connect to database 클릭
+        // connection method, hostname, port, username, password, default schema(database 이름) 항목 확인
+        // 새로운 query 생성, select문 test 해보기
+        return ;
+    }
+}
